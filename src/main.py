8c1fde173c159160bee1ccfec2b41588e817c396
GrabCrabD <105257214+GrabCrabD@users.noreply.github.com>
2023-03-24 00:25:18 +0300

    descrips, titles, internet, sms and calls

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ from dataclasses import dataclass, asdict
 from typing import Optional
 import re
 import json
-# import pickle
-# import os
 
 # url = "https://moskva.mts.ru/personal"
 url = "https://moskva.mts.ru/personal/mobilnaya-svyaz/tarifi/vse-tarifi/dla-smartfona"
@@ -17,10 +15,9 @@ class Plan:
     name: str
     price: int
     descrip: Optional[str] = None
-    sms: int = None
-    calls: int = None
-    internet: int = None
-    details: str = None
+    sms: str = None
+    calls: str = None
+    internet: str = None
 
 
 def get_cards_online():
@@ -41,36 +38,65 @@ def get_cards_online():
         #     By.XPATH, '/html/body/div[2]/div/div[1]/section/div/div/mts-tariffs-page/div[2]/div/div/div/mts-tariffs-actual/div/div[2]/mts-tariffs/div[2]/button').click()
 
         # time.sleep(3)
+        titles = []
+        for title in driver.find_elements(By.CLASS_NAME, "tariff-card__title"):
+            titles.append(title.text.strip())
+
+        descrips = []
+        for descrip in driver.find_elements(By.CLASS_NAME, "tariff-card__text"):
+            descrips.append(descrip.text.strip())
 
         soup = BeautifulSoup(driver.page_source, 'lxml')
+
     except Exception as ex:
         print(ex)
     finally:
         driver.close()
         driver.quit()
 
-    return soup.findAll('div', class_='tariff-list__item')
+    return soup.findAll('div', class_='tariff-list__item'), titles, descrips
 
 
-def make_plan(card: str) -> Optional[Plan]:
+def make_plan(card: str, title, description) -> Optional[Plan]:
 
     all_words = card.split()
 
-    name = all_words[0] if all_words[0] != 'КЕШБЭК' else all_words[2]
-
-    if name is None:
-        return
-
+    name = title
+    # убираем лишние символы переноса строк
+    descrip = re.sub(pattern='\n', repl=' ', string=description)
     price = None
-    internet = None
+    calls = "No info"
+    internet = "No info"
+    sms = "No info"
     for index, word in enumerate(all_words):
         internet_match = re.search(
-            pattern=r'(\d+)\sгб',
-            string=all_words[index-1],
-            flags=re.IGNORECASE
+            flags=0,
+            pattern=r'(?i)(\d+)\sГБ',
+            string=card
         )
         if internet_match:
-            internet = int(internet_match.group())
+            internet = internet_match.group()
+
+        if name == 'Тарифище':  # дикий костыль
+            sms = '700 SMS'
+            calls = '700 минут'
+            print(all_words)
+        else:
+            sms_match = re.search(
+                flags=0,
+                pattern=r'(?i)(\d+)\ssms|Сообщения(\d+)|SMS(\d+)',
+                string=card
+            )
+            if sms_match:
+                sms = sms_match.group()
+
+            calls_match = re.search(
+                flags=0,
+                pattern=r'(?i)(\d+)\sминут|Минуты(\d+)|(\d+)\sмин',
+                string=card
+            )
+            if calls_match:
+                calls = calls_match.group()
 
         # if '/мес' in x:
         if word in ('₽/мес', 'руб./мес.'):
@@ -80,11 +106,11 @@ def make_plan(card: str) -> Optional[Plan]:
     if price is None:
         return
 
-    return Plan(name, price, internet=internet)
+    return Plan(name, price, descrip, internet=internet, sms=sms, calls=calls)
 
 
 def main():
-    info_cards = get_cards_online()
+    info_cards, titles, descrips = get_cards_online()
     # list_of_blocks: list[tuple(str, str)] = parse_into_blocks(info_cards)
     # [
     #     (title, other),
@@ -100,17 +126,17 @@ def main():
     #        **kwargs
     #     )
 
-    with open('soup_log.txt', 'w') as file:
-        for l in info_cards:
-            file.write(l)
-            file.write('\n')
+    # with open('soup_log.txt', 'w') as file:
+    #     for l in info_cards:
+    #         file.writelines(l)
+    #         file.write('\n')
 
     with open('soup_log.txt', 'r') as file:
         info_cards = file.readlines()
 
     result = []
-    for card in info_cards:
-        plan = make_plan(card)
+    for index, card in enumerate(info_cards):
+        plan = make_plan(card, titles[index], descrips[index])
         if plan is not None:
             result.append(asdict(plan))
 
