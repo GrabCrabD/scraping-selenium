b6da77bc6edbe7367a46d9040ee3885aaf29a590
GrabCrabD <105257214+GrabCrabD@users.noreply.github.com>
2023-03-26 09:04:55 +0300

    minor fix

--- src/main.py
+++ src/main.py
@@ -23,8 +23,6 @@ class Plan:
 
 def get_cards_online():
     options = webdriver.ChromeOptions()
-    # options.add_argument(
-    #     "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15")
     driver = webdriver.Chrome(options=options)
 
     try:
